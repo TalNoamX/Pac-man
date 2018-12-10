ec7e037188cd579ef970a3e4d218dc5a76688d0c
amitai <amitaymizrahy@gmail.com>
2018-12-10 20:45:46 +0200

    amitai:edit Fruit class

--- src/GameData/Fruit.java
+++ src/GameData/Fruit.java
@@ -1,5 +1,37 @@
 package GameData;
 
-public class Fruit {
+import Geom.Geom_element;
+import Geom.Point3D;
+
+public class Fruit  {
+Point3D point;
+double Weight;
+int ID;
+public Fruit(String id,String lat,String lon,String alt,String weight ) {
+	point=new Point3D(lat,lon,alt);
+	this.setWeight(weight);
+	this.setID(id);
+}
+
+	public Geom_element getGeom() {
+		return point;
+	}
+
+	public void translate(Point3D vec) {
+		// TODO Auto-generated method stub
+	}
+	public double grtweight() {
+		return this.Weight;
+	}
+	public int getid() {
+		return this.ID;
+	}
+
+	private void setID(String id) {
+		ID=Integer.parseInt(id);
+	}
+	private void setWeight(String w) {
+		Weight=Double.parseDouble(w);
+	}
 
 }
