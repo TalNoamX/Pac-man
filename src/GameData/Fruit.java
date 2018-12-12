c3ac8018cc35e47788d901eb7ab5f9022281a80b
Oranit <Oranit@Oranit-HP>
2018-12-12 10:24:48 +0200

    Adding javadoc to Fruit

--- src/GameData/Fruit.java
+++ src/GameData/Fruit.java
@@ -2,22 +2,41 @@ package GameData;
 
 import Geom.Geom_element;
 import Geom.Point3D;
-
+/**
+ * A class that represents the fruit on the map for tha packman to eat.
+ * @author Oranit
+ * @author Tal
+ * @author Amitai
+ * 
+ */
 public class Fruit implements PackmanFruit  {
 	Point3D point;
 	double Weight;
 	int ID;
 	
-	
+	/**
+	 * A constructor that is build from Strings, to read from a CSV file.
+	 * @param id  An ID
+	 * @param lat  part of a coordinate
+	 * @param lon part of a coordinate
+	 * @param alt part of a coordinate
+	 * @param weight The fruit weight for the calculation of the score 
+	 */
 	public Fruit(String id,String lat,String lon,String alt,String weight ) {
 		point=new Point3D(lat,lon,alt);
 		this.setWeight(weight);
 		this.setID(id);
 	}
+	/**
+	 * A getter
+	 */
 	@Override
 	public Geom_element getGeom() {
 		return point;
 	}
+	/**
+	 * T
+	 */
 	@Override
 	public void translate(Point3D vec) {
 		// TODO Auto-generated method stub
