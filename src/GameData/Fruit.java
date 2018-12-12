cfabe36c7213ac04d2743d931266a6094a3df8f0
טל <talnoamb@gmail.com>
2018-12-12 12:15:53 +0200

    Tal: added timedata to packman and fruit.
    made a new class call timedata to keep the time of every packman and
    fruit.

--- src/GameData/Fruit.java
+++ src/GameData/Fruit.java
@@ -9,11 +9,12 @@ import Geom.Point3D;
  * @author Amitai
  * 
  */
-public class Fruit implements PackmanFruit  {
-	Point3D point;
-	double Weight;
-	int ID;
-	
+public class Fruit {
+	private timeData time;
+	private Point3D point;
+	private double Weight;
+	private int ID;
+
 	/**
 	 * A constructor that is build from Strings, to read from a CSV file.
 	 * @param id  An ID
@@ -23,28 +24,24 @@ public class Fruit implements PackmanFruit  {
 	 * @param weight The fruit weight for the calculation of the score 
 	 */
 	public Fruit(String id,String lat,String lon,String alt,String weight ) {
+		time = new timeData();
 		point=new Point3D(lat,lon,alt);
 		this.setWeight(weight);
 		this.setID(id);
 	}
-	/**
-	 * A getter
-	 */
-	@Override
+
+	public String GetTime() {
+		return time.getTime();
+	}
+
 	public Geom_element getGeom() {
 		return point;
 	}
-	/**
-	 * T
-	 */
-	@Override
-	public void translate(Point3D vec) {
-		// TODO Auto-generated method stub
-	}
+
 	public double getWeight() {
 		return this.Weight;
 	}
-	@Override
+
 	public int getID() {
 		return this.ID;
 	}
@@ -52,11 +49,17 @@ public class Fruit implements PackmanFruit  {
 	private void setID(String id) {
 		ID=Integer.parseInt(id);
 	}
+
 	private void setWeight(String w) {
 		Weight=Double.parseDouble(w);
 	}
+
+	public void translate(Point3D vec) {
+		// TODO Auto-generated method stub
+	}
+
 	public String toString() {
-		return "Fruit info - "+"Point: "+this.getGeom()+" ID:"+this.getID()+" Weight:"+this.getWeight();
+		return "Fruit info [ "+"Point: "+this.getGeom()+" ID:"+this.getID()+" Weight:"+this.getWeight()+"]";
 	}
 
 }
--- src/GameData/Packman.java
+++ src/GameData/Packman.java
@@ -3,34 +3,35 @@ package GameData;
 import Geom.Geom_element;
 import Geom.Point3D;
 
-public class Packman implements PackmanFruit {
+public class Packman {
+	private timeData time;
 	private Point3D point;
 	private double speed;
 	private double radius;
 	private int ID;
 
 	public Packman(String id, String lat ,String lon , String alt, String sp ,String rad) {
+		time = new timeData();
 		point = new Point3D(lat,lon,alt);
 		this.setID(id);
 		this.setRadius(rad);
 		this.setSpeed(sp);
 	}
+	public String GetTime() {
+		return time.getTime();
+	}
 
-	@Override
 	public Geom_element getGeom() {
 		return point;
 	}
-	@Override
-	public void translate(Point3D vec) {
-		// TODO check how to convert pixel to coords.
-	}
-	@Override
+
 	public int getID() {
 		return ID;
 	}
 	public double getSpeed() {
 		return speed;
 	}
+
 	public double getRadius() {
 		return radius;
 	}
@@ -38,14 +39,21 @@ public class Packman implements PackmanFruit {
 	private void setSpeed(String sp) {
 		speed = Double.parseDouble(sp);
 	}
+
 	private void setRadius(String rad) {
 		radius = Double.parseDouble(rad);
 	}
+
 	private void setID(String id) {
 		ID = Integer.parseInt(id);
 	}
+
+	public void translate(Point3D vec) {
+		// TODO check how to convert pixel to coords.
+	}
+
 	public String toString() {
-		return "Packman info - "+"Point: "+this.getGeom()+" ID:"+this.getID()+" Speed:"+this.getSpeed()+" Radius:"+this.getRadius();
+		return "Packman info [ "+"Point: "+this.getGeom()+" ID:"+this.getID()+" Speed:"+this.getSpeed()+" Radius:"+this.getRadius()+"]";
 	}
 
 }
