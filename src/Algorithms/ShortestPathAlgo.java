1e56db2a61d146ed7af1f36067e32d1d594cf109
טל <talnoamb@gmail.com>
2018-12-23 17:34:59 +0200

    Added J Doc

--- src/Algorithms/ShortestPathAlgo.java
+++ src/Algorithms/ShortestPathAlgo.java
@@ -4,30 +4,48 @@ import java.util.Iterator;
 import GameData.Game;
 import GameData.Pacman;
 import Maps.MyFrame;
-
+/**
+ * This class made to calculate the shortest path from one pacman or more. 
+ * after we have the shortest path it update every pacman's path to be as fast as possible.
+ * and when all the paths are ready with "Start" function it call the thread to move the pacmans at the same time.
+ * @author 1.Tal 
+ * @author 2.Amitai
+ * @author 3.Oranit
+ *
+ */
 public class ShortestPathAlgo {
 	private Game game;
 	private Path shortestPath;
 	private MyFrame MF;
 
-	
-	public ShortestPathAlgo(Game game,MyFrame MF) { //constructor
+	/**
+	 * constructor
+	 * @param game to get the pList and fList
+	 * @param MF to print the pacmans later 
+	 */
+	public ShortestPathAlgo(Game game,MyFrame MF) {
 		this.game=game;
 		this.pacmansPath(); // we want to split the run time of this algorithm so we call this function upon initialization.
 		this.MF=MF;
 	}
-	
-	public void Start() { //This function activate the shortest path. it take every pacman to it's fruit "physically".
+	/**
+	 * This function activate the shortest path.
+	 *  it take every pacman to it's fruit "physically" by sending threads to the ShortestPathAlgoThread class.
+	 */
+	public void Start() { 
 		Iterator<Pacman> pacIter=game.pList().iterator();
 		while(pacIter.hasNext()) { //run on the pacman arraylist
 			Pacman pac=pacIter.next();
 			Thread T = new ShortestPathAlgoThread(pac, MF);
-			T.start();
+			T.start(); // call the run function
 			}
 		MF.results();
 		}
-
-	private void pacmansPath() { //this function takes the fastest path and compare it with each pacmans path, then delete the fruits that this pacman do not eat, delete is in his own path.
+	/**
+	 * this function takes the fastest path and compare it with each pacmans path, then delete the fruits that this pacman do not eat,
+	 *  delete is in his own path.
+	 */
+	private void pacmansPath() { 
 		this.getTheShortestPath(); //call the TheShortestPath function
 		for(int i=0;i<game.pList().size();i++) { // loop that runs on the pacmans
 			int pacmanID=game.pList().get(i).getID(); //save pacman ID
@@ -48,7 +66,11 @@ public class ShortestPathAlgo {
 		}
 
 	}
-	private Path getTheShortestPath() { //This function creates the Shortest Path this the path class.
+	/**
+	 * This function creates the Shortest Path this the path class.
+	 * @return the shortestPath is the best way to split the pacmans among the fruit.
+	 */
+	private Path getTheShortestPath() { 
 		for(int i=0;i<game.pList().size();i++) { //loop that runs on the pacmans
 			Path temp = new Path(game.fList(),game.pList().get(i)); //call the "path" class the create a new pacman path
 			game.pList().get(i).setPath(temp); // set the path of every pacman. we use it in pacmansPath after.
